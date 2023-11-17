53b4699fca140c7e5afa4ba722f73c47ea17aba4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-11-17 11:00:19 +0100

    commit

--- lib/models/block_widget.dart
+++ lib/models/block_widget.dart
@@ -267,7 +267,7 @@ class _BlockWidgetState extends State<BlockWidget> {
                       if (blockProvider.stackedRowBlockValues.length > 1) {
                         blockProvider.activateGravity(context);
                       }
-                      Future.delayed(200.milliseconds, () {
+                      Future.delayed(800.milliseconds, () {
                         blockProvider.onTap(context);
                       });
                     });
