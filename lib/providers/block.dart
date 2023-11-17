70a5c1384bdd9b0250c8063332482462d1851bcf
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-11-17 08:54:23 +0100

    commit

--- lib/providers/block.dart
+++ lib/providers/block.dart
@@ -48,7 +48,7 @@ class BlockProvider extends ChangeNotifier {
 
       stackedRowBlockValues.add(currentRowBlockInts);
 
-      print("Stacked Row Block Values: ${stackedRowBlockValues.}");
+      print("Stacked Row Block Values: ${stackedRowBlockValues.map((e) => e["blockWidth"])}");
 
       if (stackedRowBlockValues.length > 1) {
         print("activating gravity");
